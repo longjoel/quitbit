2aca64b4b425703397ff6ed0618053a72feff784
Joel Longanecker <joel.longanecker@gmail.com>
2016-02-13 08:34:27 -0600

    added --rr flag to restore resolution when games are misbehaving. looking at you every single ps1 emulator ever made...

--- QuitBit.cs
+++ QuitBit.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2015, Joel Longanecker
+// Copyright (c) 2016, Joel Longanecker
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without modification, 
@@ -61,6 +61,173 @@ namespace QuitBit
         public int dwReserved2;
     }
 
+    /// <summary>
+    /// This class is used to un-screw with the screen resolution if a game is being naughty.
+    /// </summary>
+    internal sealed class WindowManager : IDisposable
+    {
+        struct POINTL
+        {
+            public Int32 x;
+            public Int32 y;
+        }
+
+        [Flags()]
+        enum DM : int
+        {
+            Orientation = 0x1,
+            PaperSize = 0x2,
+            PaperLength = 0x4,
+            PaperWidth = 0x8,
+            Scale = 0x10,
+            Position = 0x20,
+            NUP = 0x40,
+            DisplayOrientation = 0x80,
+            Copies = 0x100,
+            DefaultSource = 0x200,
+            PrintQuality = 0x400,
+            Color = 0x800,
+            Duplex = 0x1000,
+            YResolution = 0x2000,
+            TTOption = 0x4000,
+            Collate = 0x8000,
+            FormName = 0x10000,
+            LogPixels = 0x20000,
+            BitsPerPixel = 0x40000,
+            PelsWidth = 0x80000,
+            PelsHeight = 0x100000,
+            DisplayFlags = 0x200000,
+            DisplayFrequency = 0x400000,
+            ICMMethod = 0x800000,
+            ICMIntent = 0x1000000,
+            MediaType = 0x2000000,
+            DitherType = 0x4000000,
+            PanningWidth = 0x8000000,
+            PanningHeight = 0x10000000,
+            DisplayFixedOutput = 0x20000000
+        }
+
+        [StructLayout(LayoutKind.Explicit, CharSet = CharSet.Ansi)]
+        struct DEVMODE
+        {
+            public const int CCHDEVICENAME = 32;
+            public const int CCHFORMNAME = 32;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHDEVICENAME)]
+            [System.Runtime.InteropServices.FieldOffset(0)]
+            public string dmDeviceName;
+            [System.Runtime.InteropServices.FieldOffset(32)]
+            public Int16 dmSpecVersion;
+            [System.Runtime.InteropServices.FieldOffset(34)]
+            public Int16 dmDriverVersion;
+            [System.Runtime.InteropServices.FieldOffset(36)]
+            public Int16 dmSize;
+            [System.Runtime.InteropServices.FieldOffset(38)]
+            public Int16 dmDriverExtra;
+            [System.Runtime.InteropServices.FieldOffset(40)]
+            public DM dmFields;
+
+            [System.Runtime.InteropServices.FieldOffset(44)]
+            Int16 dmOrientation;
+            [System.Runtime.InteropServices.FieldOffset(46)]
+            Int16 dmPaperSize;
+            [System.Runtime.InteropServices.FieldOffset(48)]
+            Int16 dmPaperLength;
+            [System.Runtime.InteropServices.FieldOffset(50)]
+            Int16 dmPaperWidth;
+            [System.Runtime.InteropServices.FieldOffset(52)]
+            Int16 dmScale;
+            [System.Runtime.InteropServices.FieldOffset(54)]
+            Int16 dmCopies;
+            [System.Runtime.InteropServices.FieldOffset(56)]
+            Int16 dmDefaultSource;
+            [System.Runtime.InteropServices.FieldOffset(58)]
+            Int16 dmPrintQuality;
+
+            [System.Runtime.InteropServices.FieldOffset(44)]
+            public POINTL dmPosition;
+            [System.Runtime.InteropServices.FieldOffset(52)]
+            public Int32 dmDisplayOrientation;
+            [System.Runtime.InteropServices.FieldOffset(56)]
+            public Int32 dmDisplayFixedOutput;
+
+            [System.Runtime.InteropServices.FieldOffset(60)]
+            public short dmColor; // See note below!
+            [System.Runtime.InteropServices.FieldOffset(62)]
+            public short dmDuplex; // See note below!
+            [System.Runtime.InteropServices.FieldOffset(64)]
+            public short dmYResolution;
+            [System.Runtime.InteropServices.FieldOffset(66)]
+            public short dmTTOption;
+            [System.Runtime.InteropServices.FieldOffset(68)]
+            public short dmCollate; // See note below!
+            [System.Runtime.InteropServices.FieldOffset(72)]
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHFORMNAME)]
+            public string dmFormName;
+            [System.Runtime.InteropServices.FieldOffset(102)]
+            public Int16 dmLogPixels;
+            [System.Runtime.InteropServices.FieldOffset(104)]
+            public Int32 dmBitsPerPel;
+            [System.Runtime.InteropServices.FieldOffset(108)]
+            public Int32 dmPelsWidth;
+            [System.Runtime.InteropServices.FieldOffset(112)]
+            public Int32 dmPelsHeight;
+            [System.Runtime.InteropServices.FieldOffset(116)]
+            public Int32 dmDisplayFlags;
+            [System.Runtime.InteropServices.FieldOffset(116)]
+            public Int32 dmNup;
+            [System.Runtime.InteropServices.FieldOffset(120)]
+            public Int32 dmDisplayFrequency;
+        }
+
+        [DllImport("user32.dll")]
+        static extern int EnumDisplaySettings(
+         string deviceName, int modeNum, ref DEVMODE devMode);
+
+        [DllImport("user32.dll")]
+        static extern int ChangeDisplaySettings(
+              ref DEVMODE devMode, int flags);
+
+        const int ENUM_CURRENT_SETTINGS = -1;
+        const int CDS_UPDATEREGISTRY = 0x01;
+        const int CDS_TEST = 0x02;
+        const int DISP_CHANGE_SUCCESSFUL = 0;
+        const int DISP_CHANGE_RESTART = 1;
+        const int DISP_CHANGE_FAILED = -1;
+
+        private List<DEVMODE> _resolutions;
+
+        /// <summary>
+        /// Save all the settings in the constructor
+        /// </summary>
+        public WindowManager()
+        {
+            _resolutions = new List<DEVMODE>();
+
+            DEVMODE vDevMode = new DEVMODE();
+            int i = 0;
+            while (EnumDisplaySettings(null, i, ref vDevMode) == 1)
+            {
+                _resolutions.Add(vDevMode);
+
+            }
+
+        }
+
+        /// <summary>
+        /// Restore it all in the dispose
+        /// </summary>
+        public void Dispose()
+        {
+            DEVMODE dMode;
+            foreach (var d in _resolutions)
+            {
+                dMode = d;
+                ChangeDisplaySettings(ref dMode, CDS_UPDATEREGISTRY);
+            }
+        }
+    }
+
     internal sealed class Controller
     {
         [DllImport("winmm.dll")]
@@ -107,17 +274,19 @@ namespace QuitBit
         private static void Main()
         {
             Controller controller;
+            WindowManager windowManager = null;
             System.Diagnostics.Process runProgram;
             System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
             int time;
 
             {
-                string 
+                string
                     controllerString = "-1",
                     buttonsString = "",
                     execString = "",
                     paramsString = "",
                     timeString = "0",
+
                     clString = Environment.CommandLine;
 
                 string[] stringElements = clString.Split(new string[] { "--" }, StringSplitOptions.RemoveEmptyEntries);
@@ -140,6 +309,14 @@ namespace QuitBit
                             timeString = rSide;
                         else if (lSide == "contoller" || lSide == "c")
                             controllerString = rSide;
+                    }
+                    else
+                    {
+                        if (s == "rr")
+                        {
+                            windowManager = new WindowManager();
+
+                        }
                     }
                 }
 
@@ -180,12 +357,13 @@ namespace QuitBit
                         Console.WriteLine("The controller argument not used properly.");
                         error = true;
                     }
-                    if(error)
+                    if (error)
                     {
                         Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("Command      Alt   Purpose");
                         Console.ForegroundColor = ConsoleColor.White;
-                        Console.WriteLine("--buttons    --b   Button combination to close the program" + Environment.NewLine +
+                        Console.WriteLine("--rr               Restore Resolution if your emulator is not respecting the desktop" + Environment.NewLine +
+                                          "--buttons    --b   Button combination to close the program" + Environment.NewLine +
                                           "                       --b=0+8+6" + Environment.NewLine +
                                           "--exec       --e   Full path to the executable" + Environment.NewLine +
                                           "                       --e=C:\\Emulators\\nestopia.exe" + Environment.NewLine +
@@ -211,16 +389,27 @@ namespace QuitBit
                 runProgram.Start();
             }
 
-            while(true)
+            while (true)
             {
                 if (!controller.comboPressed())
                 {
                     timer.Restart();
                 }
-                else if(timer.ElapsedMilliseconds >= time)
+                else if (timer.ElapsedMilliseconds >= time)
                 {
                     try
                     {
+                        if (windowManager != null)
+                        {
+                            windowManager.Dispose();
+                            windowManager = null;
+                        }
+
+                    }
+                    catch { }
+                    try
+                    {
+
                         runProgram.Kill();
                     }
                     catch { }
